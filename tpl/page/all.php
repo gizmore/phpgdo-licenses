2fd84ae1616b0eded00ff00e56efd89030ddb924
gizmore <gizmore@wechall.net>
2022-08-09 21:42:47 +0200

    '7.0.1-r1413'

--- tpl/page/all.php
+++ tpl/page/all.php
@@ -5,6 +5,9 @@ use GDO\Core\ModuleLoader;
 use GDO\UI\GDT_Accordeon;
 use GDO\UI\GDT_Pre;
 use GDO\Core\GDO_Module;
+use GDO\Licenses\Module_Licenses;
+
+$ml = Module_Licenses::instance();
 
 $modules = ModuleLoader::instance()->getEnabledModules();
 uasort($modules, function(GDO_Module $a, GDO_Module $b) {
@@ -15,7 +18,7 @@ foreach ($modules as $module)
 {
     $name = $module->renderName();
     $title = "{$name} ({$module->license})";
-    $pre = GDT_Pre::make()->textRaw($module->displayModuleLicense());
+    $pre = GDT_Pre::make()->textRaw($ml->getModuleLicense($module));
     $accordeon = GDT_Accordeon::make();
     $accordeon->title($title);
     $accordeon->addField($pre);
