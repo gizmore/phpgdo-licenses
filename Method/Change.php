f715a3877de057588272c40372712bb4a27f53ff
gizmore <gizmore@wechall.net>
2022-09-25 17:37:58 +0200

    '7.0.1-r1611 tbs'

--- Method/Change.php
+++ Method/Change.php
@@ -3,6 +3,10 @@ namespace GDO\Licenses\Method;
 
 use GDO\Form\GDT_Form;
 use GDO\Form\MethodForm;
+use GDO\Core\GDO;
+use GDO\Core\GDT_Text;
+use GDO\Core\GDT_String;
+use GDO\Core\ModuleLoader;
 
 /**
  * Change all licenses on the fly.
@@ -14,11 +18,40 @@ final class Change extends MethodForm
 {
 	public function createForm(GDT_Form $form) : void
 	{
+		$form->addFields(
+			GDT_Text::make('text')->notNull(),
+			GDT_String::make('filename')->initial('LICENSE')->notNull(),
+		);
+		$form->addFormButtons();
 	}
 	
 	public function formValidated(GDT_Form $form)
 	{
+		$text = $form->getFormVar('text');
+		$filename = $form->getFormVar('filename');
+		$this->changeLicenses($text, $filename);
+	}
+	
+	public function changeLicenses(string $text, string $filename)
+	{
+		$modules = ModuleLoader::instance()->getEnabledModules();
+		$numWritten = 1;
+		foreach ($modules as $module)
+		{
+			if ($module->license === GDO::LICENSE)
+			{
+				$path = $module->filePath($filename);
+				file_put_contents($path, $text);
+				$numWritten++;
+			}
+		}
 		
+		# Core
+		$path = GDO_PATH . $filename;
+		file_put_contents($path, $text);
+		
+		$this->message('msg_licenses_changed', [
+			$numWritten, html($filename)]);
 	}
 	
 }
--- lang/licenses_en.php
+++ lang/licenses_en.php
@@ -3,5 +3,6 @@ namespace GDO\Licenses\lang;
 
 return [
     'licenses' => 'Licenses',
-	'md_licenses_all' => 'Pedandic license overview for this website and service.'
+	'md_licenses_all' => 'Pedantic license overview for this website and service.',
+	'msg_licenses_changed' => '%s %s files have been written.',
 ];
