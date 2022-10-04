1aeaa6c5b81276841a719a2623e5eef7e70080eb
gizmore <gizmore@wechall.net>
2022-10-04 11:47:46 +0200

    'KC'

---
+++ Method/TrackingState.php
@@ -0,0 +1,16 @@
+<?php
+namespace GDO\Licenses\Method;
+
+use GDO\Core\Method;
+
+/**
+ * @TODO Show information of this installations tracking state.
+ * @author gizmore
+ */
+final class TrackingState extends Method
+{
+	function execute()
+	{
+	}
+
+}
